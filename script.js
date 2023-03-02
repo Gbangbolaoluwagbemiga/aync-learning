73966d39f46889144d29d8233194010810c5fcbb
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2023-03-02 11:40:06 +0100

    api getting more fun

--- script.js
+++ script.js
@@ -4,43 +4,53 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
-const getCountries = function (country) {
-  const request = new XMLHttpRequest();
-  request.open(
-    'GET',
-    `https://restcountries.com/v3.1/name/${country}
-`
-  );
-  request.send();
 
-  request.addEventListener('load', function () {
-    const [data] = JSON.parse(this.responseText);
-    console.log(data.borders[0]);
-    const [we, us] = data.borders[0];
-    console.log(we);
-    console.log(data);
-
-    const html = `
+const displayCountries = country => {
+  const html = `
   <article class="country">
-    <img class="country__img" src="${data.flags.svg}" />
+    <img class="country__img" src="${country.flags.svg}" />
     <div class="country__data">
-      <h3 class="country__name">${data.name.common}</h3>
-      <h4 class="country__region">${data.region}</h4>
+      <h3 class="country__name">${country.name.common}</h3>
+      <h4 class="country__region">${country.region}</h4>
       <p class="country__row"><span>👫</span>${(
-        data.population / 1000000
+        country.population / 1000000
       ).toFixed(1)} people</p>
-      <p class="country__row"><span>🗣️</span>${data.languages?.eng}</p>
-      <p class="country__row"><span>💰</span>${data.currencies.NGN?.name}</p>
+      <p class="country__row"><span>🗣️</span>${country.languages?.eng}</p>
+      <p class="country__row"><span>💰</span>${country.currencies.NGN?.name}</p>
     </div>
   </article>`;
 
-    countriesContainer.insertAdjacentHTML('beforeend', html);
-    countriesContainer.style.opacity = 1;
-  });
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+////////////////////////////////////////
+// Outdated way of going around async
+// const getCountries = function (country) {
+//   const request = new XMLHttpRequest();
+//   request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+//   request.send();
+
+//   request.addEventListener('load', function () {
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data.languages[0]);
+//     displayCountries(data)
+//     console.log(data);
+//   });
+// };
+
+// promises on async function
+const getCountries = function (country) {
+  fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then(response => response.json())
+    .then(data => {
+      displayCountries(data[0]);
+      console.log(data[0]);
+    });
 };
 
 getCountries('Nigeria');
 getCountries('Spain');
-getCountries('italy');
 getCountries('japan');
-getCountries('portugal');
+// gettCountries('italy');
+// geCountries('portugal');
